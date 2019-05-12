5c4b6d4cc57b4c29270123e4358d6b03268f5dd8
Yuhao Ko <yhk354@nyu.edu>
2019-05-12 02:41:59 -0400

    jrijri

--- script.js
+++ script.js
@@ -9,9 +9,14 @@ const path = require('path')
 var port = process.env.PORT || 8000
 // console.log(process.env)
 
+// var client = new Client({
+//   connectionString: process.env.HEROKU_POSTGRESQL_GREEN_URL || process.env.DATABASE_URL,
+//   ssl: !!process.env.HEROKU_POSTGRESQL_GREEN_URL
+// })
+
 var client = new Client({
-  connectionString: process.env.HEROKU_POSTGRESQL_GREEN_URL || process.env.DATABASE_URL,
-  ssl: !!process.env.HEROKU_POSTGRESQL_GREEN_URL
+  connectionString: process.env.DATABASE_URL,
+  ssl: !!process.env.DATABASE_URL
 })
 
 client.connect()

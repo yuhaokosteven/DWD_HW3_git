a93713c5302724fc390d58cf8a1f10110b823137
Yuhao Ko <yhk354@nyu.edu>
2019-05-12 02:04:35 -0400

    jrijri

---
+++ public/script.js
@@ -0,0 +1,90 @@
+var express = require('express')
+var mustacheExpress = require('mustache-express')
+var bodyParser = require('body-parser')
+const {
+  Client
+} = require('pg')
+const path = require('path')
+
+var port = process.env.PORT || 8000
+// console.log(process.env)
+
+var client = new Client({
+  connectionString: process.env.HEROKU_POSTGRESQL_GREEN_URL || process.env.DATABASE_URL,
+  ssl: !!process.env.HEROKU_POSTGRESQL_GREEN_URL
+})
+
+client.connect()
+// .then(() => console.log('connect'))
+// .catch(e => console.error('connection error', err.stack))
+
+
+var app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
+app.engine('html', mustacheExpress())
+app.set('view engine', 'html')
+app.set('views', __dirname)
+
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
+
+
+
+// app.get('/all', function(req, res) {
+//   client.query(`SELECT * FROM posts`, (err, result) => {
+//     res.json(result)
+//     res.end()
+//   })
+// })
+
+app.get('/', function(req, res) {
+
+  // res.sendFile(path.join(__dirname + '/index.html'))
+
+  client.query(`SELECT * FROM posts`, (err, result) => {
+    // client.query(`SELECT * FROM posts ORDER BY id`, function(err, result)){ //for multiple inputs
+    // console.log(result)
+    // res.send('Your data is saved!')
+    if (err) throw err
+    for (let row of result.rows) {
+      console.log(JSON.stringify(row))
+    }
+    let messagesArray = result.rows
+    res.render('index', {
+      messagesArray
+    })
+  })
+})
+
+// const insertQuery = `INSERT INTO whatever (title, introduction) VALUES ($1, $2)`
+
+app.post('/post', function(req, res) {
+
+  // const title = req.body.title
+  var intro = req.body.content
+  if (intro === undefined) {
+    res.sendFile(path.join(__dirname + 'index.html'))
+  } else {
+    client.query('INSERT INTO posts (message) VALUES ($1)', [intro], function(err, result) {
+      // client.query(insertQuery, [req.body.title, req.body.introduction], function(err, result) { // for multiple inputs
+      if (err) throw err
+      res.redirect('/')
+    })
+  }
+})
+
+// var sql = "INSERT INTO customers (Title, Introduction) VALUES" + "(" + title + "," + intro + ")"
+// client.query(sql, (err, result) => {
+//   if (err) throw err
+//   console.log(ressult)
+// })
+// })
+
+app.listen(port, function() {
+  console.log("Web Server Started at port 8000")
+})
